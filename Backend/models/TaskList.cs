1d9c07ddebd22ad9e4cdfb81da5e18a5141879bd
Kervie19 <114479831+Kervie19@users.noreply.github.com>
2023-12-12 18:37:39 +0800

    Add files via upload

---
+++ Backend/models/TaskList.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class TaskList
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public bool IsArchived { get; set; } = false;
+        public virtual ICollection<TodoTask> TodoTasks { get; set; } = new List<TodoTask>();
+    }
+}
---
+++ Backend/models/TodoTask.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Models
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+        public string? Todo { get; set; }
+        public bool IsCompleted { get; set; } = false;
+    }
+}
